0f53778f43583f1a6e8553a9f9754b1f4ffc02ae
Thomas <thomashimmelev@gmail.com>
2020-11-30 14:04:34 +0100

    Updates

---
+++ precache-manifest.c69ea2fcd4c54090f9c87d0794c5b874.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffd3c46d0a508552728326419e0bb2a",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "ab673c1f97a63263f26c",
+    "url": "/deploy/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d5b90b988d495ad311ec",
+    "url": "/deploy/static/js/2.fdc148d8.chunk.js"
+  },
+  {
+    "revision": "ab673c1f97a63263f26c",
+    "url": "/deploy/static/js/main.df16e93f.chunk.js"
+  },
+  {
+    "revision": "0dcb4eec97520abc4f58",
+    "url": "/deploy/static/js/runtime~main.9c98c2a1.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/deploy/static/media/Entypo.744ce600.ttf"
+  },
+  {
+    "revision": "140c53a7643ea949007aa9a282153849",
+    "url": "/deploy/static/media/EvilIcons.140c53a7.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/deploy/static/media/Feather.ca9ce9ff.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/deploy/static/media/FontAwesome.b06871f2.ttf"
+  },
+  {
+    "revision": "92de2dbc2bf1aeb55153b5ff2f53f934",
+    "url": "/deploy/static/media/Imagecard-bedtime.92de2dbc.png"
+  },
+  {
+    "revision": "d661295ed42fe92842183c07bcd214f4",
+    "url": "/deploy/static/media/Imagecard-knowYourEnergy.d661295e.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/deploy/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "6acc7d6cffa21f6eda4baaa90e835fd2",
+    "url": "/deploy/static/media/MaterialCommunityIcons.6acc7d6c.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/deploy/static/media/MaterialIcons.a37b0c01.ttf"
+  },
+  {
+    "revision": "8e7f807ef943bff1f6d3c2c6e0f3769e",
+    "url": "/deploy/static/media/Octicons.8e7f807e.ttf"
+  },
+  {
+    "revision": "18cb71ff8f9f23f0847d50ef51fd0a91",
+    "url": "/deploy/static/media/Path_113.18cb71ff.png"
+  },
+  {
+    "revision": "41bcd05a9534d7d6f8fdb0cbcf9549b1",
+    "url": "/deploy/static/media/Shape-background.41bcd05a.png"
+  },
+  {
+    "revision": "41bcd05a9534d7d6f8fdb0cbcf9549b1",
+    "url": "/deploy/static/media/Shape-background1.41bcd05a.png"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/deploy/static/media/SimpleLineIcons.d2285965.ttf"
+  }
+]);
